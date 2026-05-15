47084c38ef5cc446432043890455b4dc0066cb9c
Tomás Sale <tomassale@hotmail.com>
2026-05-15 20:30:15 -0300

    New fix

--- client/api/main.ts
+++ client/api/main.ts
@@ -1,11 +1,12 @@
-// api/main.js
-
-module.exports = async (req: any, res: any) => {
+// api/main.ts
+export default async (req: any, res: any) => {
   try {
     //@ts-ignore: Ignoramos la falta de tipos del archivo compilado
-    const serverApp = await import('../dist/client/server/server.mjs');
+    const { app } = await import('../dist/client/server/server.mjs'); // Verifica que esta ruta coincida con tu dist
     
-    return serverApp.app()(req, res);
+    // Ejecutamos app() para obtener la instancia de Express y le pasamos req, res
+    const server = app();
+    return server(req, res);
   } catch (error) {
     console.error('Error al cargar el servidor de Angular SSR:', error);
     res.status(500).send('Error interno del servidor al cargar SSR');
