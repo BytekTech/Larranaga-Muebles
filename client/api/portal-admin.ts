978f6c4602ee38806a92f7444351a62f0fc50179
Tomás Sale <tomassale@hotmail.com>
2026-05-15 22:24:57 -0300

    Add logs for vercel logs

--- client/api/portal-admin.ts
+++ client/api/portal-admin.ts
@@ -30,7 +30,15 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
   const allowedIps = process.env['IP_SERVER'] || '127.0.0.1';
   const allowedSecret = process.env['SECRET_SERVER'];
 
+  console.log('IP de User:', userIp)
+  console.log('IPs permitidas:', allowedIps)
+  console.log('Secret valida:', allowedSecret)
+  console.log('Secret ingresada:', secret)
+  console.log('Es valido el ingreso:', isAllowedIp(userIp, allowedIps))
+
   if (secret === allowedSecret && isAllowedIp(userIp, allowedIps)) {
+    console.log('INGRESO VALIDO!')
+    // Seteamos la cookie nativamente. No usamos HttpOnly para que Angular pueda leerla.
     res.setHeader('Set-Cookie', 'access-granted=true; Path=/; Max-Age=1800; Secure; SameSite=Lax');
     return res.redirect(302, '/portal-admin');
   }
